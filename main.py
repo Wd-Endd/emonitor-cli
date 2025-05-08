591834815ba0a05d910967694d28a364289b1bf5
Wd-Endd <endercitihacker11111@gmail.com>
2025-05-08 18:13:02 +0700

    ;-;

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 
-print("Hello, World")
+DIST_DIR = "./dist"
+BUILD_DIR = "./.build"
+SCRIPT_DIR = "./script"
 
+CONTROL = {
+    name: "emonitor-cli",
+    arch: "all",
+    maimtainer: "Wd-Endd <noreply@github.com>",
+    version: 1.0,
+    homepage: "https://github.com/Wd-Endd/emonitor-cli.git",
+    depend: "none",
+    description: "Endd's first dpkg",
+    installed_size: 0,
+}
+
+# create_dist(DIST_DIR)
+# create_build(BUILD_DIR)
