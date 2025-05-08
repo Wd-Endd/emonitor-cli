4e7f17783e591933a8b4acd0d7632b904a1c0b64
Wd-Endd <endercitihacker11111@gmail.com>
2025-05-08 18:40:08 +0700

    ;-;

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ DIST_DIR = "./dist"
 BUILD_DIR = "./.build"
 SCRIPT_DIR = "./script"
 
-CONTROL = {
-    name: "emonitor-cli",
-    arch: "all",
-    maimtainer: "Wd-Endd <noreply@github.com>",
-    version: 1.0,
-    homepage: "https://github.com/Wd-Endd/emonitor-cli.git",
-    depend: "none", # default
-    description: "Endd's first dpkg",
-    installed_size: 0, # default
-}
+class Control:
+    name = "emonitor-cli",
+    arch = "all",
+    maimtainer = "Wd-Endd <noreply@github.com>",
+    version = 1.0,
+    homepage = "https://github.com/Wd-Endd/emonitor-cli.git",
+    depend = "none", # default
+    description = "Endd's first dpkg",
+    installed_size = 0, # default
 
 create_dist(DIST_DIR)
 create_build(BUILD_DIR)
--- utils/create_dist.py
+++ utils/create_dist.py
@@ -1,3 +1,4 @@
+import os
 
 def create_dist(dir):
-    a
+    os.mkdir(dir)
